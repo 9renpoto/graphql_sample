fc96cf104851c6759c4a545d3e562a77ababf228
Keisuke Kan <9renpoto@gmail.com>
2018-11-12 17:41:39 +0900

    fix(lib/graphql_example) Added adapter

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule GraphqlSample.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 4.0"},
       {:postgrex, ">= 0.0.0"},
+      {:ecto_sql, "~> 3.0"},
       {:gettext, "~> 0.11"},
       {:absinthe_ecto, "~> 0.1.0"},
       {:absinthe_plug, "~> 1.4.4"},
